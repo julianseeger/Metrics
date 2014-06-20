06b62a95817f4aadbf03286159c5cde0f75ae07a
Julian Seeger <seeger.julian@gmail.com>
2014-06-21 01:56:40 +0200

    AddProjectInteractor

---
+++ core/src/Interactor/AddProjectInteractor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Metrics\Core\Interactor;
+
+use Metrics\Core\Entity\Project;
+use Metrics\Core\Repository\ProjectRepository;
+
+class AddProjectInteractor
+{
+    /**
+     * @var ProjectRepository
+     */
+    private $projectRepository;
+
+    public function __construct(ProjectRepository $projectRepository)
+    {
+        $this->projectRepository = $projectRepository;
+    }
+
+    public function execute($name)
+    {
+        $project = new Project();
+        $project->setName($name);
+        $this->projectRepository->save($project);
+    }
+}
---
+++ core/tests/Interactor/AddProjectInteractorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Metrics\Core\Interactor;
+
+use Metrics\Core\Entity\Project;
+use Metrics\Core\Repository\ProjectRepositoryMock;
+
+class AddProjectInteractorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAddsProjectToRepository()
+    {
+        $name = 'testname';
+        $repo = new ProjectRepositoryMock();
+        $interactor = new AddProjectInteractor($repo);
+
+        $this->assertEmpty($repo->findAll());
+
+        $interactor->execute($name);
+
+        $projects = $repo->findAll();
+        $this->assertEquals(1, count($projects));
+        /** @var Project $project */
+        $project = $projects[0];
+        $this->assertInstanceOf('\\Metrics\\Core\\Entity\\Project', $project);
+        $this->assertEquals($name, $project->getName());
+    }
+}
