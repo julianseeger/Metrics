baee336d64f8e2fb9d39096123f61c1bd16a4a4c
Julian Seeger <seeger.julian@gmail.com>
2014-06-23 21:10:29 +0200

    added a test for NaviCtl::selectProject

--- web/test/spec/controllers/navi.js
+++ web/test/spec/controllers/navi.js
@@ -9,18 +9,26 @@ describe('Controller: NaviCtl', function () {
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, Api, _$httpBackend_) {
+  beforeEach(inject(function ($controller, $rootScope, Api, _$httpBackend_, ProjectScope) {
     scope = $rootScope.$new();
     _$httpBackend_.expectGET('api/projects').respond([{name: 'project1'}]);
     _$httpBackend_.expectGET('api/projects').respond([{name: 'project1'}]);
     NaviCtl = $controller('NaviCtl', {
       $scope: scope,
-      Api: Api
+      Api: Api,
+      ProjectScope: ProjectScope
     });
     _$httpBackend_.flush();
   }));
 
   it('should attach the list of projects', function () {
     expect(scope.projects.length).toBe(1);
+  });
+
+  it('should be able to change the global project', function() {
+    expect(scope.ProjectScope.project).toBe(null);
+    scope.selectProject({name: 'test'});
+    expect(scope.ProjectScope.project).not.toBe(null);
+    expect(scope.ProjectScope.project.name).toBe('test');
   });
 });
